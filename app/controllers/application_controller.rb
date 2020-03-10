ec66bf6864b324e6d3c39e876709ea28e1d9fbb0
Moaiad Alsultan <moaiadalsultan@outlook.de>
2020-03-10 10:32:21 +0100

    add the message ablillity to the app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :logged_in?
 
     def current_user
-      @current_user ||= User.find(sessions[:user_id]) if sessions[:user_id]
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
     end
 
     def logged_in?
@@ -13,6 +13,6 @@ class ApplicationController < ActionController::Base
       if !logged_in?
         flash[:error]= "You must be logged in to perform that action"
         redirect_to login_path
+      end
     end
-  end
 end
